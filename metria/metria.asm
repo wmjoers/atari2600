88f5cc6357903355c8177115e053e00dc5c042ff
John Eriksson <wmjoers@gmail.com>
2023-06-04 21:09:37 +0200

    wip

--- metria/metria.asm
+++ metria/metria.asm
@@ -27,8 +27,8 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
     MAC SetObjectXPos
-        sta WSYNC               ; Get fresh scanline
         sec                     ; Set carry flag
+        sta WSYNC               ; Get fresh scanline
 .Div15Loop                  ; Divide A with 15 by subtraction in loop
         sbc #15                 ; Subtract 15 from A
         bcs .Div15Loop          ; Loop if carry flag is set
@@ -47,6 +47,7 @@
 LOGO_BACKGROUND_COLOR = #$38
 LOGO_BACKGROUND_BW = #$08
 LOGO_COLOR = #$0E
+
 LOGO_LINE = #24
 LOGO_HEIGHT = #9
 LOGO_FADE_INITIAL_DELAY = #40
@@ -54,9 +55,6 @@ LOGO_FADE_DELAY = #20
 
 GAME_BK_BW = #$08
 GAME_PLAYER_HEIGHT = #9
-
-GAME_PLAYER_X_MIN = #2
-GAME_PLAYER_X_MAX = #132
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; RAM variables located outside ROM at address $0080
@@ -76,6 +74,7 @@ GM_PlayerYPos       ds 1
 GM_BugColorPtr      ds 2
 GM_BugXPos          ds 1
 GM_BugYPos          ds 1
+GM_PlayfieldIdx     ds 1
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Program start - Located at top of ROM at address $F000
@@ -101,9 +100,9 @@ Reset:
 
     SET_POINTER GM_BugColorPtr, GM_BUG_COLOR
 
-    lda #67
+    lda #62
     sta GM_PlayerXPos
-    lda #46
+    lda #21
     sta GM_PlayerYPos
 
     lda #10
@@ -265,6 +264,13 @@ GM_NextFrame:
     lda #1
     sta CTRLPF
 
+    lda #%00000101
+    sta NUSIZ0
+    sta NUSIZ1
+
+    lda #0
+    sta GM_PlayfieldIdx
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Wait for the remining scanlines - Total 37
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -300,10 +306,11 @@ GM_NextFrame:
 ;; Draw screen - 192 scanlines - 2 line kernel
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-    ldx #96            ; X counter contains the remaining scanlines
+    ldx #48            ; X counter contains the remaining scanlines
 
 .GM_KernelLoop:
-    ldy 0
+
+    ldy GM_PlayfieldIdx
     lda GM_PLAYFIELD,Y
     sta PF0
     iny
@@ -312,6 +319,10 @@ GM_NextFrame:
     iny
     lda GM_PLAYFIELD,Y
     sta PF2
+    iny
+    sty GM_PlayfieldIdx
+
+    sta WSYNC           ; wait for next scanline
 
     txa                 ; transfer X to A
     sec                 ; make sure carry flag is set
@@ -344,6 +355,8 @@ GM_NextFrame:
     sta GRP1            ; set graphics for player 0 slice
     lda (GM_BugColorPtr),Y       ; load player color from lookup table
     sta COLUP1          ; set color for player 0 slice
+
+    sta WSYNC           ; wait for next scanline
 
     dex
     bne .GM_KernelLoop   ; repeat next scanline until finished
@@ -397,7 +410,7 @@ GM_NextFrame:
     bit SWCHA
     bne .GM_CheckRight
 
-    lda GAME_PLAYER_X_MIN
+    lda #2
     cmp GM_PlayerXPos
     beq .GM_CheckRight
     dec GM_PlayerXPos
@@ -406,7 +419,7 @@ GM_NextFrame:
     lda #%10000000
     bit SWCHA
     bne .GM_NoInput
-    lda #132
+    lda #134
     cmp GM_PlayerXPos
     beq .GM_NoInput
     inc GM_PlayerXPos
@@ -515,13 +528,13 @@ GM_PANTS_WALK2:
 GM_BUG:
         .byte #0
         .byte #%00000000;$00
-        .byte #%10101001;$F0
-        .byte #%01011010;$F0
-        .byte #%01111100;$F0
-        .byte #%01111100;$F4
-        .byte #%01011010;$F2
-        .byte #%10101001;$F0
-        .byte #%00000000;$00        
+        .byte #%00000000;$00
+        .byte #%01010010;$F0
+        .byte #%00111100;$F0
+        .byte #%00111100;$F2
+        .byte #%01010010;$F0
+        .byte #%00000000;$00
+        .byte #%00000000;$00      
 ;---End Graphics Data---
 
 
@@ -549,12 +562,12 @@ GM_PLAYER_BW:
 GM_BUG_COLOR:
         .byte #0
         .byte #$00;
+        .byte #$00;
         .byte #$F0;
         .byte #$F0;
-        .byte #$F0;
-        .byte #$F4;
         .byte #$F2;
         .byte #$F0;
+        .byte #$00;
         .byte #$00;
 GM_BUG_BW:
         .byte #0
@@ -562,14 +575,60 @@ GM_BUG_BW:
         .byte #$00;
         .byte #$00;
         .byte #$00;
-        .byte #$04;
         .byte #$02;
         .byte #$00;
         .byte #$00;
-GM_PLAYFIELD:
-    .byte $70,$00,$00
+        .byte #$00;
 
-;---End Color Data---
+GM_PLAYFIELD:
+        .byte $F0,$FF,$FF ;|XXXXXXXXXXXXXXXXXXXX| ( 0)
+        .byte $F0,$FF,$FF ;|XXXXXXXXXXXXXXXXXXXX| ( 1)
+        .byte $F0,$51,$19 ;|XXXX X X   XX  XX   | ( 2)
+        .byte $F0,$00,$00 ;|XXXX                | ( 3)
+        .byte $70,$00,$00 ;|XXX                 | ( 4)
+        .byte $30,$00,$00 ;|XX                  | ( 5)
+        .byte $30,$00,$00 ;|XX                  | ( 6)
+        .byte $30,$00,$00 ;|XX                  | ( 7)
+        .byte $30,$00,$00 ;|XX                  | ( 8)
+        .byte $30,$00,$00 ;|XX                  | ( 9)
+        .byte $30,$00,$00 ;|XX                  | (10)
+        .byte $30,$03,$00 ;|XX        XX        | (11)
+        .byte $10,$07,$01 ;|X        XXXX       | (12)
+        .byte $10,$0F,$03 ;|X       XXXXXX      | (13)
+        .byte $10,$0F,$03 ;|X       XXXXXX      | (14)
+        .byte $30,$0F,$03 ;|XX      XXXXXX      | (15)
+        .byte $30,$0F,$03 ;|XX      XXXXXX      | (16)
+        .byte $10,$0F,$03 ;|X       XXXXXX      | (17)
+        .byte $10,$03,$00 ;|X         XX        | (18)
+        .byte $30,$03,$00 ;|XX        XX        | (19)
+        .byte $30,$03,$00 ;|XX        XX        | (20)
+        .byte $30,$03,$00 ;|XX        XX        | (21)
+        .byte $30,$00,$00 ;|XX                  | (22)
+        .byte $10,$00,$00 ;|X                   | (23)
+        .byte $10,$00,$00 ;|X                   | (24)
+        .byte $10,$00,$00 ;|X                   | (25)
+        .byte $30,$00,$00 ;|XX                  | (26)
+        .byte $30,$00,$00 ;|XX                  | (27)
+        .byte $30,$00,$80 ;|XX                 X| (28)
+        .byte $30,$00,$C0 ;|XX                XX| (29)
+        .byte $30,$00,$E0 ;|XX               XXX| (30)
+        .byte $10,$00,$E0 ;|X                XXX| (31)
+        .byte $10,$00,$E0 ;|X                XXX| (32)
+        .byte $30,$00,$E0 ;|XX               XXX| (33)
+        .byte $30,$00,$E0 ;|XX               XXX| (34)
+        .byte $30,$00,$80 ;|XX                 X| (35)
+        .byte $30,$00,$80 ;|XX                 X| (36)
+        .byte $30,$00,$80 ;|XX                 X| (37)
+        .byte $10,$00,$80 ;|X                  X| (38)
+        .byte $10,$00,$00 ;|X                   | (39)
+        .byte $10,$00,$00 ;|X                   | (40)
+        .byte $30,$00,$00 ;|XX                  | (41)
+        .byte $30,$00,$00 ;|XX                  | (42)
+        .byte $70,$00,$00 ;|XXX                 | (43)
+        .byte $F0,$00,$00 ;|XXXX                | (44)
+        .byte $F0,$C7,$00 ;|XXXXXX   XXX        | (45)
+        .byte $F0,$FF,$FF ;|XXXXXXXXXXXXXXXXXXXX| (46)
+        .byte $F0,$FF,$FF ;|XXXXXXXXXXXXXXXXXXXX| (47)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Fill the 4K ROM
