520f5d34d471c8052d79aaa67ffed62fcd2a4c20
John Eriksson <wmjoers@gmail.com>
2023-06-05 14:49:23 +0200

    wip

--- metria/metria.asm
+++ metria/metria.asm
@@ -14,7 +14,6 @@
         bne .WaitX          ; Loop until X = 0
     ENDM
 
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Contants
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -25,18 +24,19 @@ LEFT_BTN_MASK = %10000000       ; bitmask for left joystick button
 TIMER_VBLANK = 43               ; value for TIM64T vertical blank timer
 TIMER_OVERSCAN = 35             ; value for TIM64T overscan timer
 
-LOGO_BACK_COLOR = $38           ; logo mode background color - color mode
-LOGO_BACK_BW = $06              ; logo mode background color - black & white
+LOGO_BK_COLOR = $38             ; logo mode background color - color mode
+LOGO_BK_BW = $06                ; logo mode background color - black & white
 LOGO_FADE_INIT_STATE = 4        ; initial value for the logo fade in state
 LOGO_FADE_INIT_DELAY = 40       ; initial delay value before logo fades in
 LOGO_FADE_DELAY = 20            ; delay for each fade step - 20 frames/step
 
-GAME_BK_COLOR = $C8
-GAME_BK_BW = $08
-GAME_PF_COLOR = $C0
-GAME_PF_BW = $02
+GAME_BK_COLOR = $C8             ; game background color - color mode
+GAME_BK_BW = $08                ; game background color - black & white
+GAME_PF_COLOR = $C0             ; game playfield color - color mode
+GAME_PF_BW = $02                ; game playfield color - black & white
 
-GAME_PLAYER_HEIGHT = 9
+GAME_PLAYER_HEIGHT = 9          ; player sprite height
+GAME_BUG_HEIGHT = 9             ; bug sprite height
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; RAM variables located outside ROM at address $0080
@@ -49,9 +49,6 @@ LM_LogoFadeState    ds 1        ; current logo fade state
 LM_LogoFadeDelay    ds 1        ; current logo fade delay
 
 GM_PlayerPtr        ds 2
-GM_PlyAnimOfset     ds 1
-GM_PlyAnimDelay     ds 1
-
 GM_PlayerColorPtr   ds 2
 GM_PlayerXPos       ds 1
 GM_PlayerYPos       ds 1
@@ -60,6 +57,8 @@ GM_BugColorPtr      ds 2
 GM_BugXPos          ds 1
 GM_BugYPos          ds 1
 GM_PlayfieldIdx     ds 1
+
+PFCounter           ds 1
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Program start - Located at top of ROM at address $F000
@@ -89,9 +88,6 @@ Reset:
     sta GM_PlayerXPos
     lda #21
     sta GM_PlayerYPos
-    lda #0
-    sta GM_PlyAnimDelay
-    sta GM_PlyAnimOfset
 
     lda #10
     sta GM_BugXPos
@@ -130,13 +126,13 @@ LM_NextFrame:
     and #BW_MASK                ; check if black & white
     beq .LM_BWMode          
 .LM_ColorMode:
-    lda #LOGO_BACK_COLOR    
+    lda #LOGO_BK_COLOR    
     sta COLUBK                  ; set background color
     lda LM_LogoFade_Color,Y 
     sta COLUPF                  ; set logo color
     jmp .LM_SetColorDone
 .LM_BWMode:
-    lda #LOGO_BACK_BW       
+    lda #LOGO_BK_BW       
     sta COLUBK                  ; set background color
     lda LM_LogoFade_BW,Y
     sta COLUPF                  ; set logo color
@@ -147,6 +143,7 @@ LM_NextFrame:
     bne .LM_VBLankWait          ; wait until timer is done
     lda #0
     sta WSYNC                   ; get a fresh scanline
+    ; ------------------------- 
     sta VBLANK                  ; turn off VBLANK
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -156,6 +153,7 @@ LM_NextFrame:
 .LM_Top                         ; waste 68 scanlines
     ldx #68
     WAIT_X_WSYNC
+    ; -------------------------
 
 .LM_Middle                      ; logo is a total of 36 scanlines
     ldy #0                      ; Y = index to playfield bytes
@@ -163,6 +161,7 @@ LM_NextFrame:
     ldx #4;                     ; draw every logo line 4 scanlines
 .LM_LoopX:
     sta WSYNC                   ; get fresh scanline
+    ; -------------------------
     lda LM_LogoPF0,Y
     sta PF0                     ; set first part of left playfield 
     lda LM_LogoPF1,Y
@@ -188,6 +187,7 @@ LM_NextFrame:
 
 .LM_TurnOffLogo                 ; use 1 scanline to turn off logo
     lda WSYNC                   ; get a fresh scanline   
+    ; -------------------------
     lda #0                  
     sta PF0                     ; reset playfield graphics
     sta PF1
@@ -196,6 +196,7 @@ LM_NextFrame:
 .LM_Bottom
     ldx #87                     ; waste remaining 87 scanlines
     WAIT_X_WSYNC
+    ; -------------------------
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Overscan - 30 scanlines - 2280 mc
@@ -205,24 +206,26 @@ LM_NextFrame:
     lda #TIMER_OVERSCAN
     sta TIM64T                  ; set timer to 35x64 = 2240 mc
 
+.LM_CheckReset:
     lda SWCHB                   ; load console switches
     and #RESET_MASK             
     bne .LM_NoReset
     jmp Reset                   ; jump to reset if reset button has been pressed
 .LM_NoReset:
 
+.LM_CheckLeftButton:
     lda INPT4                   ; load left joystick button
     and #LEFT_BTN_MASK
-    bne .LM_NoFireButton
+    bne .LM_NoLeftButton
     sta WSYNC
     jmp GM_NextFrame            ; start game if button is pressed
-.LM_NoFireButton:
+.LM_NoLeftButton:
 
 .LM_OverscanWait:
     ldx INTIM
     bne .LM_OverscanWait        ; wait until timer is done
     sta WSYNC
-
+    ; -------------------------
     jmp LM_NextFrame
 
 
@@ -247,6 +250,7 @@ GM_NextFrame:
     jsr SetObjectXPos           ; call subroutine to set object x pos
 
     sta WSYNC                   ; geta fresh scanline 
+    ; -------------------------
     sta HMOVE                   ; apply positions offset
 
 .GM_SetColor:                   ; set correct colors
@@ -268,147 +272,124 @@ GM_NextFrame:
     sta COLUPF
     SET_POINTER GM_PlayerColorPtr, GM_PLAYER_BW_IDLE
     SET_POINTER GM_BugColorPtr, GM_BUG_BW
-.GM_SetColorDone
+.GM_SetColorDone:
+
+.GM_SetGraphics
+    lda SWCHB
+    and #%01000000
+    beq .GM_SetDress
+.GM_SetPants:
+    SET_POINTER GM_PlayerPtr, GM_PANTS_IDLE
+    jmp .GM_SetGraphicsDone
+.GM_SetDress:
+    SET_POINTER GM_PlayerPtr, GM_DRESS_IDLE
+.GM_SetGraphicsDone:
+
+    lda #96
+    sta PFCounter   
+
+    lda #1
+    sta VDELP0
 
 .GM_VBLankWait:
     ldx INTIM
     bne .GM_VBLankWait          ; wait until timer is done
     lda #0
     sta WSYNC                   ; get a fresh scanline
+    ; -------------------------
     sta VBLANK                  ; turn off VBLANK
  
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Draw screen - 192 scanlines - 2 line kernel
+;; Playfield - 192 scanlines - 14592 mc
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-    ldx #96            ; X counter contains the remaining scanlines
+.GM_PlayfieldLoop:
 
-.GM_KernelLoop:
-
-    txa                 ; transfer X to A
-    sec                 ; make sure carry flag is set
-    sbc GM_PlayerYPos   ; subtract sprite Y coordinate
-    cmp GAME_PLAYER_HEIGHT   ; are we inside the sprite height bounds?
-    bcc .GM_LoadPlayer  ; if result < SpriteHeight, call subroutine
-    lda #0              ; else, set index to 0
-.GM_LoadPlayer:
-    
-    clc
-    adc GM_PlyAnimOfset
-
+.GM_DrawPlayer:
+    lda PFCounter               ; A = current scanline in playfield
+    sec                         ; make sure carry flag is set
+    sbc GM_PlayerYPos           ; subtract sprite Y coordinate
+    cmp GAME_PLAYER_HEIGHT      ; are we inside the sprite height bounds?
+    bcc .GM_WritePlayer         ; if result < height then A contains the index
+    lda #0                      ; else, set A to 0
+.GM_WritePlayer:
     tay
-    lda (GM_PlayerPtr),Y      ; load player bitmap slice of data
+    lda (GM_PlayerPtr),Y        ; load player bitmap slice of data
+    sta GRP0                    ; set graphics for player 0 slice - delayed
+    lda (GM_PlayerColorPtr),Y   ; load player color from lookup table
+    tax
+.GM_DrawPlayerDone:
 
-    sta WSYNC           ; wait for next scanline
-
-    sta GRP0            ; set graphics for player 0 slice
-
-    lda (GM_PlayerColorPtr),Y       ; load player color from lookup table
-    sta COLUP0          ; set color for player 0 slice
-
-    txa                 ; transfer X to A
-    sec                 ; make sure carry flag is set
-    sbc GM_BugYPos   ; subtract sprite Y coordinate
-    cmp GAME_PLAYER_HEIGHT   ; are we inside the sprite height bounds?
-    bcc .GM_LoadBug  ; if result < SpriteHeight, call subroutine
-    lda #0              ; else, set index to 0
-.GM_LoadBug:
+.GM_DrawBug:
+    lda PFCounter               ; transfer X to A
+    sec                         ; make sure carry flag is set
+    sbc GM_BugYPos              ; subtract sprite Y coordinate
+    cmp GAME_PLAYER_HEIGHT      ; are we inside the sprite height bounds?
+    bcc .GM_WriteBug            ; if result < SpriteHeight, call subroutine
+    lda #0                      ; else, set index to 0
+.GM_WriteBug:
     tay
-    lda GM_BUG,Y      ; load player bitmap slice of data
+    lda GM_BUG,Y                ; load player bitmap slice of data
+    sta WSYNC                   ; wait for next scanline
+    ; ------------------------- 
+    sta GRP1                    ; set graphics for player 1 + 0 slice
+    stx COLUP0                  ; set color for player 0 slice
+    lda (GM_BugColorPtr),Y      ; load player color from lookup table
+    sta COLUP1                  ; set color for player 1 slice
+.GM_DrawBugDone:
 
-    sta WSYNC           ; wait for next scanline
+    sta WSYNC                   ; wait for end of second scanline
+    ; -------------------------  
+    dec PFCounter
+    bne .GM_PlayfieldLoop       ; repeat next scanline until finished
 
-    sta GRP1            ; set graphics for player 0 slice
-    lda (GM_BugColorPtr),Y       ; load player color from lookup table
-    sta COLUP1          ; set color for player 0 slice
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Overscan - 30 scanlines - 2280 mc
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    lda #2                      ; A = 2 = #%00000010
+    sta VBLANK                  ; Turn on VBLANK
+    lda #TIMER_OVERSCAN         
+    sta TIM64T                  ; set timer to 35x64 = 2240 mc
 
-    dex
-    bne .GM_KernelLoop   ; repeat next scanline until finished
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Handle overscan - 30 scanlines
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-.GM_OverScanWait:
-    lda #2              ; A = 2 = #%00000010
-    sta VBLANK          ; Turn on VBLANK
-    ldx #27             ; X = 30-2
-    WAIT_X_WSYNC        ; Wait for X scanlines
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Check input
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-    lda #%00000001
-    bit SWCHB
+.GM_CheckReset:
+    lda SWCHB                   ; load console switches
+    and #RESET_MASK             
     bne .GM_NoReset
-    jmp Reset
+    jmp Reset                   ; jump to reset if reset button has been pressed
 .GM_NoReset:
 
-    lda #%01000000
-    bit SWCHB
-    beq .GM_SetDress
-.GM_SetPants:
-    SET_POINTER GM_PlayerPtr, GM_PANTS_IDLE
-    jmp .GM_DificultyDone
-.GM_SetDress:
-    SET_POINTER GM_PlayerPtr, GM_DRESS_IDLE
-.GM_DificultyDone:
-    sta WSYNC
-
-    ldx #0
-
-.GM_CheckUp:
+.GM_CheckInputUp:
     lda #%00010000
     bit SWCHA
-    bne .GM_CheckDown
-    ldx #1
+    bne .GM_CheckInputDown
     inc GM_PlayerYPos
 
-.GM_CheckDown:
+.GM_CheckInputDown:
     lda #%00100000
     bit SWCHA
-    bne .GM_CheckLeft
-    ldx #1
+    bne .GM_CheckInputLeft
     dec GM_PlayerYPos
 
-.GM_CheckLeft:
+.GM_CheckInputLeft:
     lda #%01000000
     bit SWCHA
-    bne .GM_CheckRight
-
-    lda #2
-    cmp GM_PlayerXPos
-    beq .GM_CheckRight
-    ldx #1
+    bne .GM_CheckInputRight
     dec GM_PlayerXPos
 
-.GM_CheckRight:
+.GM_CheckInputRight:
     lda #%10000000
     bit SWCHA
-    bne .GM_NoInput
-    lda #134
-    cmp GM_PlayerXPos
-    beq .GM_NoInput
-    ldx #1
+    bne .GM_CheckInputDone
     inc GM_PlayerXPos
 
-.GM_NoInput:
+.GM_CheckInputDone:
 
-    txa
-    cmp #0
-    beq .GM_NoAnim
-
-    lda #GAME_PLAYER_HEIGHT
-    sta GM_PlyAnimOfset
-    jmp .GM_AnimDone
-.GM_NoAnim:
-    lda #0
-    sta GM_PlyAnimOfset
-.GM_AnimDone:
-
+.GM_OverscanWait:
+    ldx INTIM
+    bne .GM_OverscanWait        ; wait until timer is done
     sta WSYNC
+    ; -------------------------  
     jmp GM_NextFrame
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Subruotines
@@ -428,6 +409,7 @@ GM_NextFrame:
 SetObjectXPos subroutine
         sec                     ; Set carry flag
         sta WSYNC               ; Get fresh scanline
+        ; -------------------------  
 .Div15Loop                      ; Divide A with 15 by subtraction in loop
         sbc #15                 ; Subtract 15 from A
         bcs .Div15Loop          ; Loop if carry flag is set
@@ -438,7 +420,6 @@ SetObjectXPos subroutine
         sta HMP0,Y              ; Set fine position value for object HMP0+Y
         sta RESP0,Y             ; Seset rough position for object HMP0+Y
         rts
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Lookup tabes
